314da868cb79847b1fa3e22eb2c0ff78ca6f59fc
Aliaksandr Piatrovich <alex.petrovich83@gmail.com>
2022-04-20 09:27:42 +0300

    Moved the logic from the task.

--- HW20/ViewController.swift
+++ HW20/ViewController.swift
@@ -32,12 +32,11 @@ class ViewController: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = ""
     }
 
     @IBAction func generatePasswordBtnDidPressed(_ sender: Any) {
-        let randomLength = Int.random(in: 3...5)
-        textField.isSecureTextEntry = true
+        let randomLength = Int.random(in: 3...4)
+//        textField.isSecureTextEntry = true
         activityIndicator.startAnimating()
         textField.text = randomPassord(length: randomLength)
         generatePasswordBtn.isEnabled = false
@@ -45,7 +44,7 @@ class ViewController: UIViewController {
         let textToGuess = textField.text ?? ""
         
         backgroundQueue.async {
-            self.bruteForce(passwordToUnlock: textToGuess, length: randomLength)
+            self.bruteForce(passwordToUnlock: textToGuess)
             
             DispatchQueue.main.async {
                 self.label.text = self.textField.text
@@ -65,12 +64,62 @@ class ViewController: UIViewController {
       return String((0..<length).map{ _ in letters.randomElement()! })
     }
     
-    func bruteForce(passwordToUnlock: String, length: Int) {
-        var password: String = ""
-        while password != passwordToUnlock {
-            password = randomPassord(length: length)
+    func bruteForce(passwordToUnlock: String) {
+            let ALLOWED_CHARACTERS:   [String] = String().printable.map { String($0) }
+
+            var password: String = ""
+
+            // Will strangely ends at 0000 instead of ~~~
+            while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
+                password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+    //             Your stuff here
+                print(password)
+                // Your stuff here
+            }
+            
             print(password)
         }
+}
+
+
+extension String {
+    var digits:      String { return "0123456789" }
+    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
+    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
+    var letters:     String { return lowercase + uppercase }
+    var printable:   String { return digits + letters + punctuation }
+
+    mutating func replace(at index: Int, with character: Character) {
+        var stringArray = Array(self)
+        stringArray[index] = character
+        self = String(stringArray)
     }
 }
 
+func indexOf(character: Character, _ array: [String]) -> Int {
+    return array.firstIndex(of: String(character))!
+}
+
+func characterAt(index: Int, _ array: [String]) -> Character {
+    return index < array.count ? Character(array[index])
+                               : Character("")
+}
+
+func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+    var str: String = string
+
+    if str.count <= 0 {
+        str.append(characterAt(index: 0, array))
+    }
+    else {
+        str.replace(at: str.count - 1,
+                    with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
+
+        if indexOf(character: str.last!, array) == 0 {
+            str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+        }
+    }
+
+    return str
+}
